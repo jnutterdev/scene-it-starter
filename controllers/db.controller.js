116a90e7df7b25c0c51b77d22df72ce79de8432e
terribledevice <longlost@gmail.com>
2020-12-27 20:29:19 -0500

    started working on database

---
+++ controllers/db.controller.js
@@ -0,0 +1,17 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op;
+
+
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all Tutorials from the database.
+exports.findAll = (req, res) => {
+
+};
+
+exports.delete = (req, res) => {
+
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const favicon = require('serve-favicon');
+const cors = require('cors');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const PORT = process.env.PORT || 8080;
@@ -9,6 +10,12 @@ const path = require('path');
 app.use(express.urlencoded({
     extended: true
 }));
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
 
 // static files
 app.use(favicon(__dirname + '/public/img/favicon.ico'));
@@ -21,6 +28,10 @@ app.use(express.static('public'));
 app.use(expressLayouts);
 app.set('layout', './layouts/home');
 app.set('view engine', 'ejs');
+
+// database init
+const db = require("./app/models");
+db.sequelize.sync();
 
 // set main movie routes
 const movieRouter = require('./routes/routes');
